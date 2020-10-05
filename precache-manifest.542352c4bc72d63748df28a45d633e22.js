da9a3c664edd2715e352ed5eba6ef987bb67dc46
lachmed <53819069+lachmed@users.noreply.github.com>
2020-10-05 01:29:34 +0100

    Updates

---
+++ precache-manifest.542352c4bc72d63748df28a45d633e22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6925deab63c9d7936f29513b224d84f5",
+    "url": "/simple-todolist-using-react/index.html"
+  },
+  {
+    "revision": "421e4dca6c90c5bcfbe4",
+    "url": "/simple-todolist-using-react/static/css/main.ca82f09b.chunk.css"
+  },
+  {
+    "revision": "3f8202d098603ee64de4",
+    "url": "/simple-todolist-using-react/static/js/2.1fcb4b20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todolist-using-react/static/js/2.1fcb4b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "421e4dca6c90c5bcfbe4",
+    "url": "/simple-todolist-using-react/static/js/main.54a0345a.chunk.js"
+  },
+  {
+    "revision": "a845678d6639a6b21508",
+    "url": "/simple-todolist-using-react/static/js/runtime-main.544155c2.js"
+  }
+]);
